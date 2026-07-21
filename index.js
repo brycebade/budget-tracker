61167129e72a4a97a0ac232506a469871b11d68b
Bryce Bade <229314181+brycebade@users.noreply.github.com>
2026-07-21 15:58:13 +0000

    create event listener and logged account form

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+console.error("INDEX.JS IS RUNNING");
+
 const form = document.getElementById("accountForm")
+const acctName = document.getElementById("accountName")
+const acctType = document.getElementById("accountType")
+const acctBalance = document.getElementById("accountBalance")
+
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
-    console.log("Form Submitted")
+    
+    const account = {
+        name: acctName.value,
+        type: acctType.value,
+        balance: Number(acctBalance.value)
+    }
+
+    console.log(account)
+    console.log(typeof account.balance)
 })
