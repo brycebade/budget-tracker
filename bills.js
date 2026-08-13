664f14d7942dcc7a8b8abd95174cf44793e85e69
Bryce Bade <229314181+brycebade@users.noreply.github.com>
2026-08-13 16:00:13 +0000

    Render Bill Modal

--- bills.js
+++ bills.js
@@ -1,5 +1,7 @@
 import { renderLayout } from "./components/layout.js"
-import { getBills } from "./api/billsApi.js"
+import { getBills, createBill } from "./api/billsApi.js"
+import { getAccounts } from "./api/accountsApi.js"
+import { renderBillModal } from "./billModal.js"
 
 renderLayout({
     title: "Bills",
@@ -7,11 +9,66 @@ renderLayout({
     actionLabel: "Add New Bill +"
 })
 
+const actionButton = document.getElementById("pageActionButton")
+const billsContainer = document.getElementById("billsContainer")
+
+const bills = []
+const accounts = []
+
+const openBillModal = () => {
+    const modal = renderBillModal()
+
+    const cancelButton = document.getElementById("cancelBillButton")
+    const fundingAccountSelect = document.getElementById("billFundingAccount")
+    const linkedAccountSelect = document.getElementById("billLinkedAccount")
+
+    accounts.forEach((account) => {
+        if (
+            account.type === "checking" ||
+            account.type === "savings"
+        ) {
+            fundingAccountSelect.innerHTML += `
+                <option value="${account.id}">
+                    ${account.name}
+                </option>
+            `
+        }
+
+        if (
+            account.type === "credit_card" ||
+            account.type === "loan"
+        ) {
+            linkedAccountSelect.innerHTML = `
+                <option value="${account.id}"
+                    ${account.name}
+                </option>
+            `
+        }
+    })
+
+    cancelButton.addEventListener("click", () => {
+        modal.close()
+    })
+
+    modal.showModal()
+}
+
+actionButton.addEventListener("click", () => {
+    openBillModal()
+})
+
 const loadBills = async () => {
     try {
-        const savedBills = await getBills()
+        const [savedBills, savedAccounts] = await Promise.all([
+            getBills(),
+            getAccounts()
+        ])
 
-        console.log(savedBills)
+        bills.push(...savedBills)
+        accounts.push(...savedAccounts)
+
+        console.log(bills)
+
     } catch (error) {
         console.error("Bills could not be loaded:", error)
     }
