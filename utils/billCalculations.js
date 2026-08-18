31ae3f7f75c502f0da06df53dfd8c40e4c4c984c
brycebade <bryce.bade@gmail.com>
2026-08-18 06:34:05 -0500

    Created billCalculations to determinte next due date

---
+++ utils/billCalculations.js
@@ -0,0 +1,79 @@
+export const getNextBillDueDate = (bill) => {
+    if (bill.frequency === "monthly") {
+        const today = new Date()
+
+        today.setHours(0, 0, 0, 0)
+
+        const dueDay = bill.due_day
+
+        let nextDueDate = new Date(
+            today.getFullYear(),
+            today.getMonth(),
+            dueDay
+        )
+
+        if (nextDueDate < today) {
+            nextDueDate = new Date(
+                today.getFullYear(),
+                today.getMonth() + 1,
+                dueDay
+            )
+        }
+
+        return nextDueDate
+    }
+
+    if (
+        bill.frequency === "weekly" ||
+        bill.frequency === "biweekly"
+    ) {
+        const intervalDays = 
+            bill.frequency === "weekly"
+                ? 7
+                : 14
+
+        const [year, month, day] = 
+            bill.anchor_date
+                .split("T")[0]
+                .split("-")
+                .map(Number)
+
+        const anchorDate = new Date(
+            year,
+            month - 1,
+            day    
+        )
+
+        anchorDate.setHours(0, 0, 0, 0)
+
+        const today = new Date()
+
+        today.setHours(0, 0, 0, 0)
+
+        if (anchorDate >= today) {
+            return AnchorDate
+
+        const millisecondsPerDay = 
+            1000 * 60 * 60 * 24
+        }
+
+        const daysSinceAnchor = Math.floor(
+            (today - anchorDate) / millisecondsPerDay
+        )
+
+        const intervalsToNext = Math.ceil(
+            daysSinceAnchor / intervalDays
+        )
+
+        const nextDueDate = new Date(anchorDate)
+
+        nextDueDate.setDate(
+            anchorDate.getDate() +
+            (intervalsToNext * intervalDays)
+        )
+
+        return nextDueDate
+    }
+
+    return null
+}
